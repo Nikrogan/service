f1327e802a44f7338e7d38f2a04a4228d9a0df22
nikrogan <taifun22113@gmail.com>
2021-02-10 23:16:15 +0300

    Fix slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../scss/style.scss';
 import '../scss/block-nav-menu/style-nav.scss';
 
 
-if (window.innerWidth <= 320) {
+if (window.innerWidth <= 767) {
   const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1.3,
     pagination: {
